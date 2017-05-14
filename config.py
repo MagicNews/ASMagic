986c10876f9735901628329e220d01fa1caf7c44
javad Zangmaleky <mr.kingjan123@gmail.com>
2017-05-14 15:04:18 +0430

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 token = "SET_YOUR_TOKEN_HERE"
 sudos = []
 admins = []
-errors_chat = -1001134759199 #Set your errors chat(group/channel/user) id here!
-log_chat = -218978842 #Set your logs chat(group/supergroup) id here!
+errors_chat =  #Set your errors chat(group/channel/user) id here!
+log_chat =  #Set your logs chat(group/supergroup) id here!
