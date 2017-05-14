d6d124214a46ae3189dc6361c23fb3d138f8aef1
javad Zangmaleky <mr.kingjan123@gmail.com>
2017-05-14 15:11:47 +0430

    Upload install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+read -p "Do you want me to install Magic Anti Spam Bot? (Y/N): "
+
+if [ "$REPLY" != "Y" ]; then
+    echo "Exiting..."
+else
+    echo -e "\e[1;36mUpdating packages\e[0m"
+    sudo apt-get update -y
+
+    echo -e "\e[1;36mInstalling dependencies\e[0m"
+    sudo apt-get install python2.7 python-pip -y
+    
+    
+    echo -e "\e[1;36mInstalling python modules\e[0m"
+    
+    modules="requests redis"
+    for module in $modules; do
+        sudo pip install $module
+    done
+        
+    
+    echo -e "\e[1;36mFetching latest Magic Anti Spam source code\e[0m"
+    git clone https://github.com/MagicNews/ASMagic.git
+    
+    echo -e "\e[1;32mMagicAntiSpam successfully installed! Change values in config file and run ./launch.sh\e[0m"
+    echo " "
+fi
