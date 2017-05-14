255ff54609ea18a4387eaadf8f11c15e07526c7b
javad Zangmaleky <mr.kingjan123@gmail.com>
2017-05-14 15:43:58 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,9 @@ else
     
     echo -e "\e[1;36mFetching latest Magic Anti Spam source code\e[0m"
     git clone https://github.com/MagicNews/ASMagic.git
-    
+    cd ASMagic
+    sudo chmod 777 launch.sh
+    mv ./usr/local/lib/python2.7/dist-packages/telebot /
     echo -e "\e[1;32mMagicAntiSpam successfully installed! Change values in config file and run ./launch.sh\e[0m"
     echo " "
 fi
